b0b6acd9bfd350ab8260790c1aceaa8ad59ab465
Sierra <sierrabrown808@gmail.com>
2014-11-15 19:30:41 -0600

    chat messages send

--- server.js
+++ server.js
@@ -59,3 +59,8 @@ var server = http.createServer(function(req, res) {
 server.listen(3000, function() {
 	console.log('Server listening on port 3000.')
 })
+
+//Socket.io setups
+
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
